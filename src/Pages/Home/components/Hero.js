9e3360207bae9196ce5664f36c74adf90a325b4b
HansakaKaveesh <hansakakaveesh2@gmail.com>
2024-09-01 20:07:56 +0530

    edit

--- src/Pages/Home/components/Hero.js
+++ src/Pages/Home/components/Hero.js
@@ -3,10 +3,10 @@ import './Hero.css';
 
 function Hero() {
   return (
-    <section className="hero">
-      <img src="/img with blur.png" alt="Curtain" className="hero-img" />
+    <section className="Sec">
+      <img src="/img/img with blur.png" alt="Curtain" className="hero-img" />
       <div className="hero-text">
-        <img src="/Logo.png" alt="Curtain" className="logo" />
+        <img src="/img/Logo.png" alt="Curtain" className="logo" />
         <h1>Elevate Your Brand</h1>
         <p>Glorius Textile has been the go-to company since the 80's.</p>
         <a href="#" className="btn">Realize Your Vision</a>
