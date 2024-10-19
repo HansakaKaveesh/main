479efee8e257068228ca65460b1f21d961d38c65
HansakaKaveesh <hansakakaveesh2@gmail.com>
2024-10-20 00:00:04 +0530

    edit

--- src/Pages/Home/components/CategorySlider/CategorySlider.js
+++ src/Pages/Home/components/CategorySlider/CategorySlider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 import './Category.css';
-import { Link } from 'react-router-dom';
+{/*import { Link } from 'react-router-dom';*/}
 
 
 const CategorySlider = ({ categories }) => {
