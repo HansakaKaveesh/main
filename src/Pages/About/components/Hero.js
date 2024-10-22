f90ba66b375e1bf94c9e37548824508dedb19221
HansakaKaveesh <hansakakaveesh2@gmail.com>
2024-10-22 19:54:29 +0530

    edit

--- src/Pages/About/components/Hero.js
+++ src/Pages/About/components/Hero.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Hero.css'; 
 
+
 function Hero() {
   return (
     <section className="Sec">
-      <img src="/img with blur.png" alt="Curtain" className="hero-img" />
-      <div className="hero-text">
-        <img src="/pic1.png" alt="Curtain" className="logo1" />
-        <h1>About Us</h1>
-        <p>Few things about us</p>
-        
-      </div>
-    </section>
+    <img src="/img with blur.png" alt="Curtain" className="SecImg" />
+    <div className="Sec-text">
+      <img src="/pic1.png" alt="Curtain" className="logo12" />
+      <h1>About Us</h1>
+      <p>Few things about us</p>
+      
+    </div>
+  </section>
   );
 }
 
