156a88ea61f8c4f184d82762474cd80c13f6fcf5
HansakaKaveesh <hansakakaveesh2@gmail.com>
2024-11-07 10:28:34 +0530

    edit

--- src/Pages/Home/components/CategorySlider/ProductList.js
+++ src/Pages/Home/components/CategorySlider/ProductList.js
@@ -1,6 +1,6 @@
 // ProductCard.js
-import React from 'react';
-import './ProductCard.css'; // Ensure you have this CSS file
+import React from "react";
+import "./ProductCard.css"; // Ensure you have this CSS file
 
 const ProductCard = ({ product, isActive }) => {
   // Add alignment logic for item label
@@ -8,33 +8,33 @@ const ProductCard = ({ product, isActive }) => {
 
   return (
     <div className="products-card-container">
-    <div className={"products-card"}>
-      <div className="images-section">
-        <img src={product.imageSrc} alt={`Item ${product.id}`} />
-        <div className={"items-label"}>{product.title}</div>
-      </div>
-      <div className="details-section">
-        {product.description.features.length > 0 && (
-          <div>
-            <h3>Features</h3>
-            <ul>
-              {product.description.features.map((feature, index) => (
-                <li key={index}>{feature}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-        {product.description.specifications.length > 0 && (
-          <div>
-            <h3>Specifications</h3>
-            <ul>
-              {product.description.specifications.map((spec, index) => (
-                <li key={index}>{spec}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
+      <div className={"products-card"}>
+        <div className="images-section">
+          <img src={product.imageSrc} alt={`Item ${product.id}`} />
+          <div className={"items-label"}>{product.title}</div>
+        </div>
+        <div className="details-section">
+          {product.description.features.length > 0 && (
+            <div>
+              <h3>Features</h3>
+              <ul>
+                {product.description.features.map((feature, index) => (
+                  <li key={index}>{feature}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {product.description.specifications.length > 0 && (
+            <div>
+              <h3>Specifications</h3>
+              <ul>
+                {product.description.specifications.map((spec, index) => (
+                  <li key={index}>{spec}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
     </div>
     </div>
   );
